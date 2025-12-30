012816cadbaaf5311e4e3e0391764c06a0183e4f
Lakshay Kamat <lakshaykamat.dev@gmail.com>
2025-12-30 16:12:41 +0530

    Push

--- apps/server/src/expenses/expenses.module.ts
+++ apps/server/src/expenses/expenses.module.ts
@@ -3,9 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ExpensesService } from './expenses.service';
 import { ExpensesController } from './expenses.controller';
 import { Expense, ExpenseSchema } from './schemas/expense.schema';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Expense.name, schema: ExpenseSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Expense.name, schema: ExpenseSchema }]),
+    AuthModule
+  ],
   controllers: [ExpensesController],
   providers: [ExpensesService],
 })
